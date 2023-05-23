274071b29511e634f58b7e91300521c3c8918847
higorvoltz <higor.amorim@voltzmotors.com>
2023-05-23 08:59:35 -0300

    feat: include logic to update balance when action create is used

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,21 +6,25 @@ class SalesController < ApplicationController
     render json: @sales
   end
 
-  def create # needs update balance of seller | check logic of commission
-    @sale = Sale.create!(
-      amount: params[:amount],
-      transaction_type_id: params[:transaction_type_id],
-      seller_id: params[:seller_id]
-    )
-    render json: @sale
+  def create
+    sale = Sale.new(sale_params)
+    seller = params[:seller_type].constantize.find(params[:seller_id])
+    sale.seller = seller
+
+    if sale.save
+      update_seller_balance(sale)
+      render json: sale, status: :created
+    else
+      render json: sale.errors, status: :unprocessable_entity
+    end
   end
 
-  def show # ok
+  def show
     @sale = Sale.find(params[:id])
     render json: @sale
   end
 
-  def update # update fields | needs yet update balance of seller | check logic of commission
+  def update # check logic of commission
     @sale = Sale.find(params[:id])
     @sale.update(
       amount: params[:amount],
@@ -35,5 +39,29 @@ class SalesController < ApplicationController
     @sale = Sale.find(params[:id])
     @sale.destroy
     render json: @sale
+  end
+
+  private
+
+  def sale_params
+    params.permit(
+      :sale_item_id,
+      :amount,
+      :productor_affiliated_id,
+      :transaction_type_id
+    )
+  end
+
+  def seller
+    if transaction_type_id == 1
+      @seller ||= Productor.find(seller_id).first
+    end
+  end
+
+  def update_seller_balance(sale)
+    if sale.transaction_type_id == 1 || sale.transaction_type_id == 2
+      seller = sale.seller
+      seller.update!(balance: seller.balance + sale.amount)
+    end
   end
 end

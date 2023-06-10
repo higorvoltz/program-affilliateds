9dd4ff9856932534b9673dd2d5ad7cb633321ef3
higorvoltz <higor.amorim@voltzmotors.com>
2023-06-10 17:34:27 -0300

    fix: adjust api v2 clients

--- app/controllers/api/v2/clients_controller.rb
+++ app/controllers/api/v2/clients_controller.rb
@@ -5,47 +5,47 @@ module Api
     # crud clients
     class ClientsController < ApplicationController
       def index
-        clients = ClientReader.call(params)
+        @clients = ClientReader.call(params)
         if @clients.present?
-          render json: clients, status: :ok
+          render json: @clients, status: :ok
         else
           render json: { error: 'Clients not found' }, status: :not_found
         end
       end
 
       def show
-        client = ClientReader.call(params)
+        @client = ClientReader.call(params)
         if @client.present?
-          render json: client, status: :ok
+          render json: @client, status: :ok
         else
           render json: { error: 'Client not found' }, status: :not_found
         end
       end
 
       def create
-        client = ClientCreator.call(client_params)
-        if client.save
-          render json: client, status: :created
+        @client = ClientCreator.call(client_params)
+        if @client.save
+          render json: @client, status: :created
         else
-          render json: client.errors, status: :unprocessable_entity
+          render json: @client.errors, status: :unprocessable_entity
         end
       end
 
       def update
-        client = ClientUpdater.call(params[:id], client_params)
-        if client.update(client_params)
-          render json: client, status: :ok
+        @client = ClientUpdater.call(params[:id], client_params)
+        if @client.update(client_params)
+          render json: @client, status: :ok
         else
-          render json: client.errors, status: :unprocessable_entity
+          render json: @client.errors, status: :unprocessable_entity
         end
       end
 
       def destroy
-        client = ClientDestroyer.call(params[:id])
-        if client.destroy
-          render json: client, status: :ok
+        @client = ClientDestroyer.call(params[:id])
+        if @client.destroy
+          render json: @client, status: :ok
         else
-          render json: client.errors, status: :unprocessable_entity
+          render json: @client.errors, status: :unprocessable_entity
         end
       end
 
